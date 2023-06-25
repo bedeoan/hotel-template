c0b39537cdfed6b5428effbed3c0c76044d95507
Bedeoan Raul <bedeoan.raul@gmail.com>
2023-06-25 19:21:08 +0300

    fix gallery

--- pages/Gallery.vue
+++ pages/Gallery.vue
@@ -14,6 +14,10 @@
           v-if="config.public.GALERY_2_NAME"
           :label="config.public.GALERY_2_NAME"
         ></el-tab-pane>
+        <el-tab-pane
+          v-if="config.public.GALERY_3_NAME"
+          :label="config.public.GALERY_3_NAME"
+        ></el-tab-pane>
       </el-tabs>
     </div>
     <div v-if="tab == '0'" class="flex justify-center">
@@ -25,6 +29,9 @@
     <div v-if="tab == '2'" class="flex justify-center">
       <ImageGallery :images="GALERY_2.images" />
     </div>
+    <div v-if="tab == '3'" class="flex justify-center">
+      <ImageGallery :images="GALERY_3.images" />
+    </div>
   </div>
 </template>
 
@@ -34,4 +41,5 @@ const tab = ref("0");
 const { data: GALERY_0 } = await useFetch(config.public.GALERY_0_LINK);
 const { data: GALERY_1 } = await useFetch(config.public.GALERY_1_LINK);
 const { data: GALERY_2 } = await useFetch(config.public.GALERY_2_LINK);
+const { data: GALERY_3 } = await useFetch(config.public.GALERY_3_LINK);
 </script>
