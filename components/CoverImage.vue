80796c81112e93c24c3e816ec247e89d68559779
Bedeoan Raul <bedeoan.raul@gmail.com>
2023-06-26 12:46:52 +0300

    fix

--- components/CoverImage.vue
+++ components/CoverImage.vue
@@ -9,24 +9,25 @@
       <div v-if="!$device.isMobile">
         <img
           class="w-full"
-          src="https://www.teleptean.ro/articleimages/noutati/despre-noi-04.jpg"
+          src="https://teleptean.s3.eu-west-3.amazonaws.com/2019_08_04_504c3814e9.jpg"
           alt=""
         />
 
         <div class="flex pa-2">
           <img
             class="w-1/3"
-            src="https://teleptean.s3.eu-west-3.amazonaws.com/DSC_00683_1_6db8970f0f.jpg"
+            src="https://teleptean.s3.eu-west-3.amazonaws.com/small_big_pensiunea_teleptean_ocna_sugatag_qzd6458vg1am_278e0d6ad4.jpg"
             alt=""
           />
           <img
             class="w-1/3"
-            src="https://www.teleptean.ro/images/pool/piscinaa.jpg"
+            src="https://teleptean.s3.eu-west-3.amazonaws.com/2018_12_02_7173cc23b7.jpg"
             alt=""
           />
+   
           <img
             class="w-1/3"
-            src="https://www.teleptean.ro/images/pool/piscina.jpg"
+            src="https://teleptean.s3.eu-west-3.amazonaws.com/small_pasted_image_0_35f5d670f4.png"
             alt=""
           />
         </div>
