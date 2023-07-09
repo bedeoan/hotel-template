9bce02010117d4f126537fa6b25c0f48237fe10b
Bedeoan Raul <bedeoan.raul@gmail.com>
2023-07-09 06:28:00 +0300

    fix: iphone 12 exceptions

--- components/BeHappy.vue
+++ components/BeHappy.vue
@@ -4,13 +4,13 @@
       <img class="coverimg" :src="config.public.behappyCoverImg">
       <div class="overlay" />
       <div class="overlaytext">
-        <div class="text-3xl" style="margin-top: 50px">
+        <div class="text-2xl">
           {{ config.public.behappy }}
         </div>
         <div class="my-5 px-2">
           {{ config.public.behappyDetails }}
         </div>
-        <div class="mb-5">
+        <div class="mb-2">
           {{ config.public.behappyDetails2 }}
         </div>
         <nuxt-link to="/contact">
@@ -48,29 +48,14 @@ const config = useRuntimeConfig()
   z-index: 2; /* Specify a stack order in case you're using a different order for other elements */
   cursor: pointer; /* Add a pointer on hover */
 }
-@media only screen and (min-width: 600px) {
-  .overlayMobile {
-    position: absolute; /* Sit on top of the page content */
-    display: none; /* Hidden by default */
-    width: 100%; /* Full width (cover the whole page) */
-    height: 50vh; /* Full height (cover the whole page) */
-    top: 0;
-    left: 0;
-    right: 0;
-    display: block;
-    bottom: 0;
-    background-color: rgba(0, 0, 0, 0.861); /* Black background with opacity */
-    z-index: 2; /* Specify a stack order in case you're using a different order for other elements */
-    cursor: pointer; /* Add a pointer on hover */
-  }
-}
 .overlaytext {
   position: absolute; /* Sit on top of the page content */
   display: none; /* Hidden by default */
   width: 100%; /* Full width (cover the whole page) */
-  height: 100%; /* Full height (cover the whole page) */
-  top: 1vh;
-  right: 0;
+  height: 30vh; /* Full height (cover the whole page) */
+  top: 50%;
+  transform: translate(-50%, -50%); /* This is important to truly center the child */
+  left: 50%;
   display: block;
   bottom: 0;
   z-index: 5; /* Specify a stack order in case you're using a different order for other elements */
